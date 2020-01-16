2456a0ac34b978a9fa6b742993daf9430f02e07d
angelonc <chris.angeloni@gmail.com>
2020-01-16 13:37:23 -0500

    some changes to DRC stimulus functions and a backup for estNLFR

--- stimuli/DRC/makeDRCAmps_scaled.m
+++ stimuli/DRC/makeDRCAmps_scaled.m
@@ -1,15 +1,19 @@
-function [amps, db] = makeDRCAmps_scaled(n,Mu,Sd,nTones,blockSamps,baseAmp)
+function [amps, db] = makeDRCAmps_scaled(n,Mu,Sd,nTones,blockSamps,baseAmp,seed)
 
 %% function [amps, db] = makeDRCAmps(n,Mu,Sd,nTones,blockSamps,baseAmp)
 % generates noise patterns of different contrasts, drawn from
 % two uniform distributions
 amps = [];
 db = [];
+if exist('seed','var')
+    rng(seed);
+end
 for j = 1:n
     % make some dB and amplitude values per frequency
     MU = Mu;
     SD = Sd(mod(j-1,length(Sd))+1);
-    dBs = unifrnd(MU-SD,MU+SD,nTones,blockSamps(j));
+    dBs = unifrnd(-1,1,nTones,blockSamps(j));
+    dBs = (dBs*Sd) + MU;
     db = [db dBs];
     a = baseAmp .* 10 .^ ((dBs-70)./20);
     amps = [amps a];
